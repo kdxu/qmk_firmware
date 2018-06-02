2621c8f32fee87d793f06bcb2d4a82c45d95023c
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-06-03 07:06:39 +0900

    carefully quit

--- keyboards/minidox/keymaps/kdxu/keymap.c
+++ keyboards/minidox/keymaps/kdxu/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
  *                  |      | LOWER|      |    |      | RAISE| Shift|
-*                   `-------------|  E/K |    |      |------+------.
+ *                  `-------------|  E/K |    |      |------+------.
  *                                |      |    |      |
  *                                `------'    `------'
  */
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
   KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      KC_MINS, KC_UNDS, KC_EQL,  KC_LBRC, KC_RBRC, \
   KC_LCTL, KC_GRV,  _______, KC_LALT, KC_LGUI,      _______, _______, _______, KC_BSLS,  KC_QUOT, \
-                    _______, _______, EISU_KANA,     _______, _______, KC_LSFT                   \
+                    _______, LOWER, EISU_KANA,     _______, _______, KC_LSFT                   \
 ),
 
 /* Lower
@@ -96,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |   (  |   )  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Esc | Quit |Close |Reload|      |           |   -  |   _  |   +  |   {  |   }  |
+ * |  Esc |      |Close |Reload| Find |           |   -  |   _  |   +  |   {  |   }  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |   ~  | Copy | Paste|  GUI |           |      |      |      |   |  |   "  |
+ * |      |   ~  | Copy | Paste|      |           |      |      |      |   |  |   "  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
  *                  |      | LOWER|      |    |      | RAISE| Shift |
@@ -108,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, TD(TD_LPLN), TD(TD_RPLN), \
-  TD(TD_ALF), QUIT, CLS, RLD, _______,      KC_MINS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
+  TD(TD_ALF), _______, CLS, RLD, LGUI(KC_F),      KC_MINS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
   _______, KC_TILD, CP, PST, KC_LGUI, _______, _______, _______, KC_PIPE,  KC_DQT, \
-                    _______, _______, _______,    KC_ENT,  _______, KC_LSFT                    \
+                    QUIT, _______, _______,    KC_ENT,  _______, KC_LSFT                    \
 ),
 
 /* Adjust (Lower + Raise)
@@ -174,8 +174,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_ADJUST);
         register_code(KC_LGUI);
       } else {
-        unregister_code(KC_LGUI);
         layer_off(_ADJUST);
+        unregister_code(KC_LGUI);
       }
       return false;
       break;
