41e4282f8f1122ea57a1d10ee3840ffb7b739b71
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-06-03 23:50:55 +0900

    Misc

--- keyboards/minidox/keymaps/kdxu/keymap.c
+++ keyboards/minidox/keymaps/kdxu/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  Tab | Left | Down |  Up  | Right|           |   -  |   _  |   =  |   [  |   ]  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Ctrl|   `  |      |  Alt | GUI  |           |      |      |      |   \  |   '  |
+ * |  Ctrl|   `  |      |  Alt |      |           | GUI  |      |      |   \  |   '  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
  *                  |      | LOWER|      |    |      | RAISE| Shift|
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
   KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      KC_MINS, KC_UNDS, KC_EQL,  KC_LBRC, KC_RBRC, \
-  KC_LCTL, KC_GRV,  _______, KC_LALT, KC_LGUI,      _______, _______, _______, KC_BSLS,  KC_QUOT, \
+  KC_LCTL, KC_GRV,  _______, KC_LALT, _______,      KC_LGUI, _______, _______, KC_BSLS,  KC_QUOT, \
                     _______, LOWER, EISU_KANA,     _______, _______, KC_LSFT                   \
 ),
 
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  Esc |      |      |      |      |           |   -  |   _  |   +  |   {  |   }  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |   ~  |      |      |      |      |           |      |   ;  |   /  |   |  |   "  |
+ * |   ~  |      |      |      |      |           |  GUI |   ;  |   /  |   |  |   "  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,------,-------------.
  *                  |      | LOWER|      |    |      | RAISE| Shift |
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, TD(TD_LPLN), TD(TD_RPLN), \
   TD(TD_ALF), _______, _______, _______, _______,   KC_MINS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, \
-  KC_TILD, _______, _______, _______, _______,      _______, TD(CT_CLN), KC_SLSH, KC_PIPE,  KC_DQT, \
+  KC_TILD, _______, _______, _______, _______,      KC_LGUI, TD(CT_CLN), KC_SLSH, KC_PIPE,  KC_DQT, \
                     _______, _______, _______,    KC_ENT,  _______, KC_LSFT                    \
 ),
 
