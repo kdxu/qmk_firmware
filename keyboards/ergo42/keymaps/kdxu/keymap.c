cfae24c14abc3a97208c29d1b1ad35294e40287e
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-06-12 19:16:41 +0900

    文字位置修正

--- keyboards/ergo42/keymaps/kdxu/keymap.c
+++ keyboards/ergo42/keymaps/kdxu/keymap.c
@@ -30,16 +30,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    * |      | Ctrl |   A  |   S  |   D  |   F  |  G   |   |  H   |   J  |   K  |   L  |   ;  |   '  |      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
-   * |      | Shift|   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |  UP  |      |
+   * |      | Shift|   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   UP |  /   |      |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * |      |      |      |  Alt |  Gui |LOWER |Space |   | Bksp | RAISE|   -  |   =  | LEFT | DOWN | RIGHT|
+   * |      |      |   -  |  Alt |  Gui |LOWER |Space |   | Bksp | RAISE|  GUI | LEFT | DOWN | RIGHT|
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
     KC_ESC,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_GRV, MO(_MISC), \
     XXXXXXX,  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, XXXXXXX, \
-    XXXXXXX,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP,  XXXXXXX, \
-    XXXXXXX,  XXXXXXX, XXXXXXX, KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_BSPC,  RAISE,   KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT \
+    XXXXXXX,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,   KC_M,    KC_COMM, KC_DOT, KC_UP, KC_SLSH,  XXXXXXX, \
+    XXXXXXX,  XXXXXXX, KC_MINS, KC_LALT, KC_LGUI, LOWER,   KC_SPC,   KC_BSPC,RAISE,   KC_LGUI, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX \
   ),
 
   /* RAISE
--- keyboards/helix/rev2/keymaps/kdxu/keymap.c
+++ keyboards/helix/rev2/keymaps/kdxu/keymap.c
@@ -70,18 +70,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Esc  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  /   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Enter |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |  Up  |Enter |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust| Esc  | Alt  | GUI  | EISU |Lower | Space| Bksp |Raise | KANA | Left | Down |  Up  |Right |
+   * |Adjust| Esc  | Alt  | GUI  | EISU |Lower | Space| Bksp |Raise | KANA |   -  | Left |  Down|Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, KC_LANG2, LOWER, KC_SPC, KC_BSPC, RAISE, KC_LANG1,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_SLSH, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, KC_ENT, \
+      ADJUST,  KC_ESC,  KC_LALT, KC_LGUI, KC_LANG2, LOWER, KC_SPC, KC_BSPC, RAISE, KC_LANG1,    KC_MINS, KC_LEFT, KC_DOWN,   KC_RGHT \
       ),
 
 
