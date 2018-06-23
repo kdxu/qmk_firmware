3e33b3d2b04984b502e0027c21d722b0b2d0a6a6
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-06-24 04:13:45 +0900

    Minidox

---
+++ keyboards/minidox/keymaps/kdxu/config.h
@@ -0,0 +1,5 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#define MASTER_RIGHT
+#endif
--- keyboards/minidox/keymaps/kdxu/keymap.c
+++ keyboards/minidox/keymaps/kdxu/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [QWERTY] = LAYOUT( \
   MI_Q,    KC_W,    KC_E,    KC_R,    V_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    ALCLN, \
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    COMM, DOT,  GSLSH, \
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    COMM,   KC_DOT,  GSLSH, \
                     KC_LCTL, MO(LOWER), KC_SPC,      KC_BSPC, MO(RAISE), OSM(MOD_LSFT)   \
 ),
 
