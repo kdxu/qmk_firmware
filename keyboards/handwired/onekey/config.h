ad974f44f0d6a023b2489ace0ab7309a54eca5c6
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-06-29 19:48:38 +0900

    vim

--- keyboards/handwired/onekey/config.h
+++ keyboards/handwired/onekey/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 1
 #define MATRIX_COLS 1
 
-#define MATRIX_COL_PINS {  B0 }
+#define MATRIX_COL_PINS {  B1 }
 #define MATRIX_ROW_PINS {  D0 }
 #define UNUSED_PINS
 
